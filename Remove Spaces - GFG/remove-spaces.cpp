2b38328f7411ff41bec4de3e37f59e3351e47dc6
Pratik Rajput <110167551+prrajput1199@users.noreply.github.com>
2023-05-15 01:04:15 +0530

    Added solution - LeetHub

---
+++ Remove Spaces - GFG/remove-spaces.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        
+    for(int i=0;i<s.length();i++){
+         if(s[i]==32){
+           s.erase(i,1);
+           i--;
+       }
+    }
+       return s;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
