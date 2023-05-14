ae9000f6b2754eab52ba9232da2b94106c65ea40
Pratik Rajput <110167551+prrajput1199@users.noreply.github.com>
2023-05-15 01:14:59 +0530

    Added solution - LeetHub

---
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string revStr(string S) {
+     int i=0;
+     int j=S.length()-1;
+     while(i<=j){
+         swap(S[i],S[j]);
+         i++;
+         j--;
+     }
+     return S;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.revStr(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
