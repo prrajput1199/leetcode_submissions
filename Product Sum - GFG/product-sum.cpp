f097a26dbec1f455722f5c6960acc5331f72bf6a
Pratik Rajput <110167551+prrajput1199@users.noreply.github.com>
2023-05-28 00:27:26 +0530

    Added solution - LeetHub

---
+++ Product Sum - GFG/product-sum.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution {
+  public:
+    long long int countDigits(long long int a, long long int b) { 
+        long long ans=a*b;
+        int count=0;
+        while(ans!=0){
+            int digit=ans%10;
+            ans=ans/10;
+            count++;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int a,b;
+        cin >> a >> b;
+        Solution ob;
+        cout << ob.countDigits(a, b) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
